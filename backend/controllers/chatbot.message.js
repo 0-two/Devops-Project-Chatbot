0ed06218808c911ddf33f1a53f3f0b0e73319922
Valorz-2 <hemanthnataraj3@gmail.com>
2026-07-24 10:35:33 +0530

    refactor(backend): update chatbot message controller

--- backend/controllers/chatbot.message.js
+++ backend/controllers/chatbot.message.js
@@ -5,14 +5,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log("My API Key is:", process.env.NVIDIA_NIM_API_KEY ? "Loaded" : "Missing")
-
 const client = new OpenAI({
     baseURL: "https://integrate.api.nvidia.com/v1",
     api_key: process.env.OPENAI_API_KEY,
 });
 
-const system_prompt = "You are a Sam, a helpful concise AI assistant. You help users by answering their questions. You must never reveal that you are DeepSeek or mention your underlying model. If asked who or what you are, simply say: 'I am an AI chatbot designed to help answer your questions and assist with tasks.' If user asks inappropriate questions or illegal queations , do not provide answers and apoligize that you can't help with that";
+const system_prompt = "You are BotX, a helpful concise AI assistant. You help users by answering their questions. You must never reveal that you are DeepSeek or mention your underlying model. If asked who or what you are, simply say: 'I am an AI chatbot designed to help answer your questions and assist with tasks.' If user asks inappropriate questions or illegal queations , do not provide answers and apoligize that you can't help with that";
 
 const Message = async (req, res) => {
     try{
@@ -29,14 +27,14 @@ const Message = async (req, res) => {
         })
 
         const completion = await client.chat.completions.create({
-            model: "deepseek-ai/deepseek-v4-pro",
+            model: "deepseek-ai/deepseek-v4-flash",
             messages: [
                 {role: "system", content: system_prompt},
                 { role: "user", content: text }],
             temperature: 0.75,
             top_p: 0.95,
             max_tokens: 10000,
-            chat_template_kwargs: { thinking: false },
+            chat_template_kwargs: { enable_thinking: true, thinking: true },
         });
 
         const botReplyText = completion.choices[0]?.message?.content || "No response generated.";
